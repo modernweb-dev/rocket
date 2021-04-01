67ba29d45a11478b77eacc07fd09015de37eb4ff
Benny Powers <web@bennypowers.com>
2021-04-01 20:01:49 +0300

    feat(navigation): add no-redirects attribute
    By default, the sidebar nav redirects clicks on category headings to
    their first child.
    
    To disable those redirects, override
    `_includes/_joiningBlocks/_layoutSidebar/sidebar/20-navigation.njk`
    and add the `no-redirects` attribute to the `<rocket-navigation>`
    element.

--- packages/navigation/src/RocketNavigation.js
+++ packages/navigation/src/RocketNavigation.js
@@ -1,16 +1,45 @@
 /**
+ * Debounce a function
+ * @template {(this: any, ...args: any[]) => void} T
+ * @param  {T}       func      function
+ * @param  {number}  wait      time in milliseconds to debounce
+ * @param  {boolean} immediate when true, run immediately and on the leading edge
+ * @return {T}                 debounced function
+ */
+function debounce(func, wait, immediate) {
+  /** @type {number|undefined} */
+  let timeout;
+  return /** @type {typeof func}*/ (function () {
+    let args = /** @type {Parameters<typeof func>} */ (/** @type {unknown}*/ (arguments));
+    const later = () => {
+      timeout = undefined;
+      if (!immediate) func.apply(this, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(this, args);
+  });
+}
+
+/**
  * @typedef {object} NavigationListItem
  * @property {HTMLElement} headline
  * @property {HTMLAnchorElement} anchor
  * @property {number} top
  */
 
+/**
+ * @element rocket-navigation
+ * @attr {Boolean} no-redirects - if set, will not redirect to first child of nav category when clicking on category header.
+ */
 export class RocketNavigation extends HTMLElement {
   constructor() {
     super();
     /** @type NavigationListItem[] */
     this.list = [];
-    this.__scrollHandler = this.__scrollHandler.bind(this);
+    this.__clickHandler = this.__clickHandler.bind(this);
+    this.__scrollHandler = debounce(this.__scrollHandler.bind(this), 25, true);
     this.__isSetup = false;
   }
 
@@ -20,27 +49,7 @@ export class RocketNavigation extends HTMLElement {
     }
     this.__isSetup = true;
 
-    this.addEventListener('click', ev => {
-      const el = /** @type {HTMLElement} */ (ev.target);
-      if (el.classList.contains('anchor')) {
-        const anchor = /** @type {HTMLAnchorElement} */ (el);
-        ev.preventDefault();
-        this.dispatchEvent(new Event('close-overlay', { bubbles: true }));
-        // wait for closing animation to finish before start scrolling
-        setTimeout(() => {
-          const parsedUrl = new URL(anchor.href);
-          document.location.hash = parsedUrl.hash;
-        }, 250);
-      }
-      const links = el.parentElement?.querySelectorAll('ul a');
-      if (links && links.length > 1) {
-        const subLink = /** @type {HTMLAnchorElement} */ (links[1]);
-        if (!subLink.classList.contains('anchor')) {
-          ev.preventDefault();
-          subLink.click();
-        }
-      }
-    });
+    this.addEventListener('click', this.__clickHandler);
 
     const anchors = /** @type {NodeListOf<HTMLAnchorElement>} */ (this.querySelectorAll(
       'li.current a.anchor',
@@ -57,10 +66,39 @@ export class RocketNavigation extends HTMLElement {
       }
     }
 
-    // TODO: debounce
-    window.addEventListener('scroll', this.__scrollHandler);
+    window.addEventListener('scroll', this.__scrollHandler, { passive: true });
 
     this.__scrollHandler();
+  }
+
+  /**
+   * @param  {Event} ev
+   */
+  __clickHandler(ev) {
+    const el = /** @type {HTMLElement} */ (ev.target);
+    if (el.classList.contains('anchor')) {
+      const anchor =
+        el instanceof HTMLAnchorElement
+          ? el
+          : /** @type{HTMLAnchorElement} */ (el.querySelector('a.anchor'));
+      ev.preventDefault();
+      this.dispatchEvent(new Event('close-overlay', { bubbles: true }));
+      // wait for closing animation to finish before start scrolling
+      setTimeout(() => {
+        const parsedUrl = new URL(anchor.href);
+        document.location.hash = parsedUrl.hash;
+      }, 250);
+    }
+    if (!this.hasAttribute('no-redirects')) {
+      const links = el.parentElement?.querySelectorAll('ul a');
+      if (links && links.length > 1) {
+        const subLink = /** @type {HTMLAnchorElement} */ (links[1]);
+        if (!subLink.classList.contains('anchor')) {
+          ev.preventDefault();
+          subLink.click();
+        }
+      }
+    }
   }
 
   __scrollHandler() {
