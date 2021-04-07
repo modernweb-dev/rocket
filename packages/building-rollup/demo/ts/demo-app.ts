054c76d8430e3301924829a2406f4cef817cb7fe
qa46hx <Gerjan.van.Geest@ing.com>
2021-04-07 13:47:31 +0200

    feat: upgrade to lit major prerelease

--- packages/building-rollup/demo/ts/demo-app.ts
+++ packages/building-rollup/demo/ts/demo-app.ts
@@ -1,4 +1,5 @@
-import { LitElement, html, customElement } from 'lit-element';
+import { LitElement, html } from 'lit-element';
+import { customElement } from 'lit-element/decorators.js';
 
 const msg: string = 'TypeScript demo works';
 
