d196356061dd7886df96a41852b329e40b8907cb
Benny Powers <web@bennypowers.com>
2021-07-09 03:20:11 +0300

    feat(plugins-manager): further improve types

--- packages/plugins-manager/types/main.d.ts
+++ packages/plugins-manager/types/main.d.ts
@@ -3,20 +3,22 @@ type AnyFn = (...args: any[]) => any;
 export interface MetaPlugin<F = AnyFn> {
   name: string;
   plugin: F extends (options?: infer O) => P ? F : any;
-  options?: F extends (options: infer O) => P ? O : any;
+  options?: /* prettier-ignore */ (
+      F extends (eleventyConfig: any, options?: infer O) => void ? O
+    : F extends (options: infer O) => P ? O
+    : any
+  );
 }
 
 export interface MetaPluginWrapable extends MetaPlugin {
   __noWrap?: boolean;
 }
 
-interface AddPluginOptions {
+export type AddPluginOptions<T> = MetaPlugin<T> & {
   how?: 'after' | 'before' | 'fixed';
   location?: 'top' | 'bottom' | string;
-}
+};
 
 type AddPluginFn = (plugins: MetaPlugin[]) => MetaPlugin[];
 
-export type AddPluginType = <F>(
-  metaPluginAndOptions: MetaPlugin<F> & AddPluginOptions,
-) => AddPluginFn;
+export type AddPluginType = <F>(metaPluginAndOptions: AddPluginOptions<F>) => AddPluginFn;
