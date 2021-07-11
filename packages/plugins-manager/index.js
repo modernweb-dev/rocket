61bb700c3e962ecc815e88f18c1d61b226bb8c4b
Benny Powers <web@bennypowers.com>
2021-07-11 15:57:24 +0300

    fix(plugins-manager): dist types

--- packages/plugins-manager/index.js
+++ packages/plugins-manager/index.js
@@ -1,4 +1,6 @@
 /** @typedef {import('./types/main').MetaPlugin} MetaPlugin */
+/** @typedef {import('./types/main').AddPluginFn} AddPluginFn */
+/** @typedef {import('./types/main').AddPluginType} AddPluginType */
 
 export { addPlugin } from './src/addPlugin.js';
 export { adjustPluginOptions } from './src/adjustPluginOptions.js';
