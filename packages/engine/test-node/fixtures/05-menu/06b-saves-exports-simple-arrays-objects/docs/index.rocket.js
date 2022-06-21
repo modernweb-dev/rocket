8be6df9625ec6c80e8cfbd2adefefb2a551423f4
Thomas Allmer <d4kmor@gmail.com>
2022-06-21 18:54:18 +0200

    chore: failing test simple page exports to pageTree

---
+++ packages/engine/test-node/fixtures/05-menu/06b-saves-exports-simple-arrays-objects/docs/index.rocket.js
@@ -0,0 +1,13 @@
+/* START - Rocket auto generated - do not touch */
+export const sourceRelativeFilePath = 'index.rocket.js';
+/* END - Rocket auto generated - do not touch */
+export const tags = ['web', 'javascript'];
+export const publishDate = new Date('2020-10-15');
+export const authors = [
+  {
+    name: 'Thomas',
+    age: 10,
+  },
+];
+
+export default () => '<h1>Welcome</h1>';
