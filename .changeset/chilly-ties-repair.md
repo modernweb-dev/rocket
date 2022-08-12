9e2b3ff098822c6da236e28c565771d0519d9785
Thomas Allmer <d4kmor@gmail.com>
2022-08-12 22:18:33 +0200

    fix(engine): make sure inputDir public folder wins over plugins public folders

---
+++ .changeset/chilly-ties-repair.md
@@ -0,0 +1,5 @@
+---
+'@rocket/engine': patch
+---
+
+Make sure user provided content in the folder `site/public/*` wins over public folders content provided by plugins.
--- packages/engine/test-node/10-plugins.test.js
+++ packages/engine/test-node/10-plugins.test.js
@@ -4,23 +4,42 @@ import { setupTestEngine } from './test-helpers.js';
 
 const { expect } = chai;
 
-class MyPlugin {
-  static publicFolder = new URL(
-    './fixtures/10-plugins/01-add-public-files/plugin-add-to-public/preset/__public',
-    import.meta.url,
-  ).pathname;
-}
-
 describe('Plugins', () => {
-  it('add plugin with custom public files', async () => {
+  it('01: add plugin with custom public files', async () => {
+    class TestPlugin01 {
+      static publicFolder = new URL(
+        './fixtures/10-plugins/01-add-public-files/plugin-add-to-public/preset/__public',
+        import.meta.url,
+      ).pathname;
+    }
     const { build, outputExists } = await setupTestEngine(
       'fixtures/10-plugins/01-add-public-files/docs',
       {
-        setupPlugins: [addPlugin(MyPlugin)],
+        setupPlugins: [addPlugin(TestPlugin01)],
       },
     );
     await build();
 
     expect(outputExists('added-via-plugin.txt')).to.be.true;
+  });
+
+  it('02: add plugin with custom public files', async () => {
+    class TestPlugin02 {
+      static publicFolder = new URL(
+        './fixtures/10-plugins/02-input-folder-public-always-wins/plugin-add-to-public/preset/__public',
+        import.meta.url,
+      ).pathname;
+    }
+    const { build, readOutput } = await setupTestEngine(
+      'fixtures/10-plugins/02-input-folder-public-always-wins/docs',
+      {
+        setupPlugins: [addPlugin(TestPlugin02)],
+      },
+    );
+    await build();
+
+    expect(readOutput('added-via-plugin-and-input-public.txt')).to.equal(
+      'from input public folder\n',
+    );
   });
 });
